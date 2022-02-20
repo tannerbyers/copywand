1a80dec55945cf7781acb2f77a364f5c379f7474
Tanner Byers <tanner.byers@availity.com>
2022-02-19 23:03:32 -0500

    added mongodb and s3

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,33 @@
 var express = require("express");
 const { takeScreenshot } = require("../utils.js");
-
+const { connectToDatabase } = require("../mongodb.js");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.sendFile("index.html")
+  res.sendFile("index.html");
 });
 
 /* GET home page. */
 router.get("/api/:url(*)", async function (req, res, next) {
   var url = req.params.url;
+  let { db } = await connectToDatabase();
+
+  let existingURL = await db.collection("screenshots").findOne({ url: url });
+
+  if (existingURL) {
+    res.send(existingURL.screenshotUrl);
+    return;
+  }
+
   const screenshot = await takeScreenshot(url);
-  const imgurl = (req.protocol + "://" + req.get("host") + screenshot).replace(
-    "./public",
-    ""
-  );
-  res.send(imgurl);
+
+  db.collection("screenshots").insertOne({
+    url: url,
+    screenshotUrl: screenshot,
+  });
+
+  res.send(screenshot);
 });
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -1,5 +1,39 @@
-const { nanoid } = require("nanoid");
+// const { ID, SECRET, BUCKET_NAME } = require("./constants.js");
 const puppeteer = require("puppeteer");
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
+});
+
+const uploadFile = async (fileName) => {
+  // Read content from the file
+  const customFilePath = `./public/images/${fileName}`;
+  const fileContent = fs.readFileSync(customFilePath);
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName, // File name you want to save as in S3
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket
+  const data = await s3
+    .upload(params, function (err, data) {
+      if (err) {
+        throw err;
+      }
+    })
+    .promise();
+  return data.Location;
+};
 
 exports.takeScreenshot = async (url) => {
   let browser = await puppeteer.launch({
@@ -8,10 +42,18 @@ exports.takeScreenshot = async (url) => {
   });
   let page = await browser.newPage();
   await page.goto(url);
-  var filename = url.replace(/[^a-z0-9]/gi, "_").toLowerCase();
-  const customFilePath = `./public/images/${filename}.jpg`;
+  var fileName = uuidv4() + ".jpg";
+  const customFilePath = `./public/images/${fileName}`;
   await page.screenshot({ path: customFilePath, type: "jpeg" });
   await page.close();
   await browser.close();
-  return customFilePath;
+  // Will look at deleting file later
+  // try {
+  //   fs.unlinkSync(customFilePath);
+  //   //file removed
+  // } catch (err) {
+  //   console.error(err);
+  // }
+  const awsURL = await uploadFile(fileName);
+  return awsURL;
 };
